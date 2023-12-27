d9b238f385bbb07761b39798804f25b32f6e2fdf
kouskous08 <tarekelfassihlafaoui@gmail.com>
2023-12-27 22:41:34 +0100

    d

--- test.py
+++ test.py
@@ -1,34 +1,46 @@
+from PIL import Image
 import cv2
 import numpy as np
 
+def traiter_image(chemin_image):
+    # Charger l'image
+    image = cv2.imread(chemin_image)
+
+    # Convertir en niveaux de gris
+    gris = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Appliquer un flou pour réduire le bruit
+    flou = cv2.GaussianBlur(gris, (5, 5), 0)
+
+    return flou
+
+def detecter_mouvement(image_precedente, image_actuelle):
+    # Soustraction d'arrière-plan
+    difference = cv2.absdiff(image_precedente, image_actuelle)
+
+    # Seuiller l'image pour obtenir les zones de mouvement
+    _, seuil = cv2.threshold(difference, 30, 255, cv2.THRESH_BINARY)
+
+    # Trouver les contours des zones de mouvement
+    contours, _ = cv2.findContours(seuil, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Dessiner les contours sur l'image d'origine (à des fins de visualisation)
+    image_resultante = image_actuelle.copy()
+    cv2.drawContours(image_resultante, contours, -1, (0, 255, 0), 2)
+
+    return image_resultante
+
 # Chemin de l'image
-image_path = 'C:\\Users\\hp\\Desktop\\connectmobile\\image'
-output_path = 'C:\\Users\\hp\\Desktop\\connectmobile\\image'
-def analyse_carreau(nom_image):
-	# Charger l'image depuis le fichier
-	image = cv2.imread(f"{image_path}\\{nom_image}")
+chemin_image = 'C:\\Users\\hp\\Desktop\\connectmobile\\image\\copy_b.jpg'
 
-	# Convertir l'image en niveaux de gris
-	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# Traitement de l'image
+image_precedente = traiter_image(chemin_image)
+image_actuelle = traiter_image(chemin_image)
 
-	# Définir les plages de couleurs que vous souhaitez détecter (noir et blanc)
-	lower_black = 0
-	upper_black = 100
+# Détection des mouvements
+image_mouvement = detecter_mouvement(image_precedente, image_actuelle)
 
-	lower_white = 150
-	upper_white = 255
-
-	# Créer les masques pour chaque plage de couleurs
-	mask_black = cv2.inRange(gray, lower_black, upper_black)
-	mask_white = cv2.inRange(gray, lower_white, upper_white)
-
-	# Appliquer les masques à l'image d'origine
-	result_black = cv2.bitwise_and(gray, gray, mask=mask_black)
-	result_white = cv2.bitwise_and(gray, gray, mask=mask_white)
-
-	# Enregistrer les images résultantes
-	cv2.imwrite(f'{output_path}\\noir_b.jpg', result_black)
-	cv2.imwrite(f'{output_path}\\blanc_b.jpg', result_white)
-
-analyse_carreau("copy_b.jpg")
-
+# Afficher l'image résultante avec les contours de mouvement
+cv2.imshow("Détection de mouvement", image_mouvement)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
